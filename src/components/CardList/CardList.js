026e39202b06a215c4c7fb2537a16df711b8e81e
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-03-01 18:48:23 -0600

    remove Card component an dcycle through the list in CardList

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,14 +1,33 @@
 import { React } from "react";
 import "./CardList.css";
-import Card from "../Card/Card.js";
 import drinksList from "../../utils/drinksList";
 
 export default function CardList(props) {
+  let fiveDrinks = [];
+  let thisDrinksList = drinksList.slice(0, 5);
+  thisDrinksList.forEach((drink, index) => {
+    fiveDrinks.push(
+      <li key={index}>
+        <input type="radio" name="slide" id={`c${index}`} checked />
+        <label for={`c${index}`} className="card">
+          <div className="row">
+            <div className="icon">{drink.name}</div>
+            <div className="description">
+              <h4>{drink.name}</h4>
+              <p>{drink.tags}</p>
+            </div>
+          </div>
+        </label>
+      </li>
+    );
+  });
 
   return (
     <div className="wrapper">
       <div className="container__cards">
-        <Card  />
+      <div className="card">
+        <ul>{fiveDrinks}</ul>
+      </div>
         <div className="container__card_current-recipe closed"></div>
       </div>
     </div>
