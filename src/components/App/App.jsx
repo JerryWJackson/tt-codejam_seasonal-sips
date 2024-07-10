97cd86584d1c45fec43c48b2152c29136241eeaa
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-07-10 14:49:30 -0500

    roough implementation of season switcher

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,7 @@ import Home from "../Home/Home";
 import Main from "../Main/Main";
 // eslint-disable-next-line
 import Footer from "../Footer/Footer";
+import { constants } from "../utils/constants";
 
 export default function App() {
   const [hasTag, setHadTag] = useState(false);
@@ -17,6 +18,7 @@ export default function App() {
   const [address, setAddress] = useState("");
   const [pin, setPin] = useState("");
   const [notFound, setNotFound] = useState(false);
+  const [currentSeason, setCurrentSeason] = useState("spring");
 
   const handleScroll = (elementId) => {
     setIsLocated(true);
@@ -33,15 +35,79 @@ export default function App() {
     }, 100);
   };
 
+  const seasonalElements = constants.seasonalElements;
+  const seasons = constants.seasons;
+
+  const time = new Date();
+  const curMonth = time.getMonth() + 1;
+
+  if (curMonth == 12 || curMonth == 1 || curMonth == 2) {
+    seasonalElements.forEach((element) => {
+      if (element.classList.contains("spring")) {
+        element.classList.remove("spring");
+      }
+      if (element.classList.contains("summer")) {
+        element.classList.remove("summer");
+      }
+      if (element.classList.contains("fall")) {
+        element.classList.remove("fall");
+      }
+      element.classList.add("winter");
+    });
+  } else if (curMonth == 3 || curMonth == 4 || curMonth == 5) {
+    seasonalElements.forEach((element) => {
+      if (element.classList.contains("winter")) {
+        element.classList.remove("winter");
+      }
+      if (element.classList.contains("summer")) {
+        element.classList.remove("summer");
+      }
+      if (element.classList.contains("fall")) {
+        element.classList.remove("fall");
+      }
+      element.classList.add("spring");
+    });
+  } else if (curMonth == 6 || curMonth == 7 || curMonth == 8) {
+    seasonalElements.forEach((element) => {
+      if (element.classList.contains("spring")) {
+        element.classList.remove("spring");
+      }
+      if (element.classList.contains("winter")) {
+        element.classList.remove("winter");
+      }
+      if (element.classList.contains("fall")) {
+        element.classList.remove("fall");
+      }
+      element.classList.add("summer");
+    });
+  } else {
+    seasonalElements.forEach((element) => {
+      if (element.classList.contains("spring")) {
+        element.classList.remove("spring");
+      }
+      if (element.classList.contains("summer")) {
+        element.classList.remove("summer");
+      }
+      if (element.classList.contains("winter")) {
+        element.classList.remove("winter");
+      }
+      element.classList.add("fall");
+    });
+  }
+
   return (
-    <div className="App">
-      <Home
-        setIsLocated={setIsLocated}
-        handleScroll={handleScroll}
-        setAddress={setAddress}
-        setPin={setPin}
-      />
-      <Main />
+    <div className="page">
+      <CurrentSeasonContext>
+        <div className="App">
+          <Home
+            setIsLocated={setIsLocated}
+            handleScroll={handleScroll}
+            setAddress={setAddress}
+            setPin={setPin}
+          />
+          <Main />
+        </div>
+      </CurrentSeasonContext>
     </div>
   );
 }
